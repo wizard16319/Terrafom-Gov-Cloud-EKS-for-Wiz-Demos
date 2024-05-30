93d4bdd2739fa73c5cc574002e25a815f0c5b092
Greg <gregory.thomas@wiz.io>
2024-05-30 09:03:01 -0400

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,14 @@
+output "cluster_name" {
+  description = "The name of the EKS cluster"
+  value       = module.eks.cluster_id
+}
+
+output "cluster_endpoint" {
+  description = "The endpoint of the EKS cluster"
+  value       = module.eks.cluster_endpoint
+}
+
+output "cluster_security_group_id" {
+  description = "The security group ID of the EKS cluster"
+  value       = module.eks.cluster_security_group_id
+}
